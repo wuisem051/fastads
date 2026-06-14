d5c37f39c4c0986823c9726f9e71fa9b64c647e6
wuisem abou <43572427+wuisem051@users.noreply.github.com>
2026-06-14 16:55:48 -0400

    security: move Firebase credentials to env vars (VITE_FIREBASE_*), add .gitignore and .env.example files

--- web-client/src/firebase.js
+++ web-client/src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApa-6wSGsdXIvcZxg7l4wkmrM1bVx1Xyc",
-    authDomain: "fastads-f9fb5.firebaseapp.com",
-    projectId: "fastads-f9fb5",
-    storageBucket: "fastads-f9fb5.firebasestorage.app",
-    messagingSenderId: "755333782841",
-    appId: "1:755333782841:web:6a88e8d716d2653848dce8",
-    measurementId: "G-V3M3LQE2N3"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
